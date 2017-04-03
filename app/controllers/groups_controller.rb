7379b9838afb52dd381bacb65c25dd73e2b13f2c
“AlexWong” <and751738167@gmail.com>
2017-04-03 19:28:26 +0800

    using scope to write order by created_at to group's post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,8 +7,8 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def show
     @group = Group.find(params[:id])
-    @posts = @group.posts.order("created_at DESC")
-  end
+    @posts = @group.posts.recent
+    end
 
 
    def edit
